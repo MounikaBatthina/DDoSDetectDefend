af123726ed5cc9ad655e06b590ab37210832911f
Ashok Aratikatla <ashok942@users.noreply.github.com>
2017-05-16 15:06:45 -0700

    Updated proxy with null check

--- proxy.py
+++ proxy.py
@@ -1,14 +1,17 @@
-# Proxy script to redirect the traffic
 import socket
 import select
 import time
 import sys
+import sqlite3
+
+conn = sqlite3.connect('detect.db')
 
 # Changing the buffer_size and delay, you can improve the speed and bandwidth.
 # But when buffer get to high or delay go too down, you can broke things
 buffer_size = 4096
 delay = 0.0001
 forward_to = ('192.168.216.128', 80)
+
 
 class Forward:
     def __init__(self):
@@ -21,6 +24,7 @@ class Forward:
         except Exception as e:
             print(e)
             return False
+
 
 class TheServer:
     input_list = []
@@ -55,18 +59,31 @@ class TheServer:
         clientsock, clientaddr = self.server.accept()
         if forward:
             print(clientaddr, "has connected")
-            self.input_list.append(clientsock)
-            self.input_list.append(forward)
-            self.channel[clientsock] = forward
-            self.channel[forward] = clientsock
+
+            c = conn.cursor()
+            t = (clientaddr[0],)
+            c.execute('SELECT count_diff FROM detection WHERE ip=?', t)
+            count = c.fetchone()
+            print(str(count[0])+': count')
+            if not count:
+                if(abs(count[0] < 100)):
+                    self.input_list.append(clientsock)
+                    self.input_list.append(forward)
+                    self.channel[clientsock] = forward
+                    self.channel[forward] = clientsock
+            else:
+                self.input_list.append(clientsock)
+                self.input_list.append(forward)
+                self.channel[clientsock] = forward
+                self.channel[forward] = clientsock
         else:
-            print("Can't establish connection with remote server.",)
+            print("Can't establish connection with remote server.")
             print("Closing connection with client side", clientaddr)
             clientsock.close()
 
     def on_close(self):
         print(self.s.getpeername(), "has disconnected")
-        #remove objects from input_list
+        # remove objects from input_list
         self.input_list.remove(self.s)
         self.input_list.remove(self.channel[self.s])
         out = self.channel[self.s]
@@ -81,13 +98,14 @@ class TheServer:
     def on_recv(self):
         data = self.data
         # here we can parse and/or modify the data before send forward
-        print(data)
+        # print(data)
         self.channel[self.s].send(data)
 
+
 if __name__ == '__main__':
-        server = TheServer('', 80)
-        try:
-            server.main_loop()
-        except KeyboardInterrupt:
-            print("Ctrl C - Stopping server")
+    server = TheServer('', 80)
+    try:
+        server.main_loop()
+    except KeyboardInterrupt:
+        print("Ctrl C - Stopping server")
 sys.exit(1)
